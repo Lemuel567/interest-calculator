a116a0aaea156104be57c3645590338d45e4f25a
Lemuel567 <lemuelbenyarku11@gmail.com>
2026-01-10 23:56:33 +0000

    Updated code

--- day2.js
+++ day2.js
@@ -1,14 +1,14 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let principal = Number(prompt("Enter the principal amount: "));
+let principal = Number(prompt("Enter the principal : "));
 let rate = Number(prompt("Enter the rate of interest (%): "));
-let time = Number(prompt("Enter the number of years: "));
+let time = Number(prompt("Enter the num of years: "));
 
 let interest = (principal * rate * time) / 100;
 let totalAmount = principal + interest;
 
-// Use console.log instead of alert for terminal output
+
 console.log("Simple Interest: " + interest);
 console.log("Total Amount after " + time + " years: " + totalAmount)
 
