3f066bc2e01de4b8d35c6e17d57f57f01c6b0b78
Lemuel567 <lemuelbenyarku11@gmail.com>
2026-01-11 23:26:37 +0000

    Update authentication logic

--- day2.js
+++ day2.js
@@ -1,8 +1,8 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let principal = Number(prompt("Enter the principal : "));
-let rate = Number(prompt("Enter the rate of interest (%): "));
+let principal = Number(prompt("Enter the principal amount : "));
+let rate = Number(prompt("Enter the rate of interest : "));
 let time = Number(prompt("Enter the num of years: "));
 
 let interest = (principal * rate * time) / 100;
