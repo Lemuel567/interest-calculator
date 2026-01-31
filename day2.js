10e280876d67a4acc8bbcfca1d2d7c9e8d1e474c
Lemuel567 <lemuelbenyarku11@gmail.com>
2026-01-31 02:17:28 +0000

    Update package.json and fix interest logic

--- day2.js
+++ day2.js
@@ -1,14 +1,18 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let principal = Number(prompt("Enter the principal : "));
-let rate = Number(prompt("Enter the rate  : "));
+let principal = Number(prompt("Enter the principal amount : "));
+let rate = Number(prompt("Enter the rate of interest : "));
 let time = Number(prompt("Enter the number of years: "));
 
+while(time < 0){
+    console.log("Years cannot be negative");
+    time = Number(prompt("Enter the number of years: "));
+}
 let interest = (principal * rate * time) / 100;
 let totalAmount = principal + interest;
 
 
-console.log("Simple Interest: " + interest);
+console.log(" The Simple Interest is: " + interest);
 console.log("Total Amount after " + time + " years: " + totalAmount)
 
